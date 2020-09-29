3d58e4b30259ef01438bd5a7e46cdbe09921ca0b
anaonao <1029866144@qq.com>
2020-09-29 20:26:49 +0800

    购物车Dao层和Service层接口

---
+++ src/main/java/com/tjetc/dao/CartDao.java
@@ -0,0 +1,59 @@
+package com.tjetc.dao;
+
+import com.tjetc.domain.Cart;
+import com.tjetc.util.Page;
+
+public interface CartDao {
+
+    /**
+     * 添加商品到购物车
+     * @param cart
+     * @return
+     */
+    int add(Cart cart);
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    int delete(int id);
+
+    /**
+     * 修改
+     * @param cart
+     * @return
+     */
+    int update(Cart cart);
+
+    /**
+     * 查询所有
+     * @return
+     */
+    Page<Cart> selectAll();
+    int countAll();
+
+    /**
+     * 根据订单编号查询
+     * @param orderId
+     * @return
+     */
+    Cart selectById(String orderId);
+
+    /**
+     * 根据用户名查询
+     * @param name
+     * @return
+     */
+    Page<Cart> selectByName(String name);
+    int countByName(String name);
+
+    /**
+     * 根据用户名模糊查询
+     * @param name
+     * @return
+     */
+    Page<Cart> selectLikeName(String name);
+    int countLikeName(String name);
+
+}
---
+++ src/main/java/com/tjetc/service/CartService.java
@@ -0,0 +1,57 @@
+package com.tjetc.service;
+
+import com.tjetc.domain.Cart;
+import com.tjetc.util.Page;
+
+public interface CartService {
+    /**
+     * 添加商品到购物车
+     * @param cart
+     * @return
+     */
+    int add(Cart cart);
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    int delete(int id);
+
+    /**
+     * 修改
+     * @param cart
+     * @return
+     */
+    int update(Cart cart);
+
+    /**
+     * 查询所有
+     * @return
+     */
+    Page<Cart> findAll();
+    int countAll();
+
+    /**
+     * 根据订单编号查询
+     * @param orderId
+     * @return
+     */
+    Cart findById(String orderId);
+
+    /**
+     * 根据用户名查询
+     * @param name
+     * @return
+     */
+    Page<Cart> findByName(String name);
+    int countByName(String name);
+
+    /**
+     * 根据用户名模糊查询
+     * @param name
+     * @return
+     */
+    Page<Cart> findLikeName(String name);
+    int countLikeName(String name);
+}
